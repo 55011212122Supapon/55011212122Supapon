012db6656ba0815678ddec078c432fdb0b14351f
55011212047Tae <i mac@msu>
2014-09-03 15:43:15 +0700

    work
    work

--- DictionaryZoo.playground/section-1.swift
+++ DictionaryZoo.playground/section-1.swift
@@ -2,26 +2,36 @@ class TheZoo{
     
     let zoo : String
     let eat : String
-    let feed : [String:String]
+    let feed : [String:String]=["frog":"fly","Lion":"meat","Dog":"meat","Monkey":"banana","fish":"bat"]
     
     init(zoo : String,eat : String){
         self.zoo = zoo
         self.eat = eat
-        feed = [zoo:eat]
-    }
-    
-    func ZooEat()->[String:String]{
-        var zooeat:[String:String] = ["frog":"fly","Lion":"meat","Dog":"meat","Monkey":"banana","fish":"bat",]
-        zooeat ("\(zooeat)")= feed
-        println("\(zooeat)")
-        return feed
-    }
-    
-   // func returnZoo() -> [String : String]{
         
-    //}
+        //feed = [zoo:eat]
+    }
+    
+    func ZooEat()->String{
+       var eatzoo =feed[zoo]
+        //println("\(eatzoo)")
+       //eatzoo("\(eatzoo)") =
+
+        return eatzoo!
+    }
+    
+    func MyZoo(anzoo:String){
+    
+        
+    }
     
 }
 
-let Zoo = TheZoo(zoo: "fish", eat: "tree")
+let Animal = TheZoo(zoo: "fish", eat: "bat")
+Animal.ZooEat()
+//Animal.add("Cat")
+//Animal.MyZoo()
+
+if (true){
+    println("\(Animal)")
+}
 
